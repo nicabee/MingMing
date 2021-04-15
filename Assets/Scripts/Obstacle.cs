81dd448c25c86d5591495c6b854a30ad508ad465
Monica <monicabee358@gmail.com>
2021-04-15 15:32:55 +0800

    added snowflakes, arranged score, and lives counter

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -16,7 +16,7 @@ public class Obstacle : MonoBehaviour {
         if (other.CompareTag("Player")) {
             other.GetComponent<Player>().health--;
             other.GetComponent<Player>().camAnim.SetTrigger("shake");
-            Instantiate(effect, transform.position, Quaternion.identity);
+           // Instantiate(effect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }   
     }
