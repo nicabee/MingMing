5902deb8e4493a9c99b19962e8aa0b62b2221100
Monica <monicabee358@gmail.com>
2021-04-19 17:17:09 +0800

    Changed fonts, adjusted convos, added pause button

---
+++ Assets/Scripts/PauseResume.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseResume : MonoBehaviour {
+
+	public GameObject PauseScreen;
+
+	bool GamePaused;
+	// Use this for initialization
+	void Start () {
+		GamePaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GamePaused){
+			Time.timeScale = 0;
+		}else{
+			Time.timeScale = 1;
+		}
+	}
+
+	public void PauseGame(){
+
+		GamePaused = true;
+		PauseScreen.SetActive(true);
+	}
+
+	public void ResumeGame(){
+
+		GamePaused = false;
+		PauseScreen.SetActive(false);
+	}
+}
