7422f87d2d23cf8cccdf45d249b4a12adeaac9d5
Monica <monicabee358@gmail.com>
2021-05-02 17:50:33 +0800

    Added FadeIn/Out Transitions for GAME levels 1-3

--- Assets/Scripts/Playmgt.cs
+++ Assets/Scripts/Playmgt.cs
@@ -7,8 +7,17 @@ using UnityEngine.SceneManagement;
 
 public class Playmgt : MonoBehaviour {
 
-    public void NextGame(){
+	public Animator Anim;
+	public float transitionTime = 1f;    
+	public void NextGame(){
 		Debug.Log("Clicked4");
+	//	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+		StartCoroutine(Fade());
+	}
+
+	IEnumerator Fade(){
+		Anim.SetTrigger("Start");
+		yield return new WaitForSeconds(transitionTime);
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
 	}
 }
