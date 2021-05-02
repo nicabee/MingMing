b15c133a3e4266362139b2f4ebcfd7ec86ee4ba8
Monica <monicabee358@gmail.com>
2021-05-02 17:11:36 +0800

    Added transition from main menu towards next scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+	public Animator Anim;
+//	public Image Img;
+	public float transitionTime = 1f;
 	public void PlayGame(){
 		Debug.Log("Clicked2");
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	//	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+		StartCoroutine(Fade());
 	}
 
+	IEnumerator Fade(){
+		Anim.SetTrigger("Start");
+		yield return new WaitForSeconds(transitionTime);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	}
 	public void QuitGame(){
 		Debug.Log("Quit!");
 		Application.Quit();
