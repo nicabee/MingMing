059dd925b719e14975328754260bcf27d2955f8d
Monica <monicabee358@gmail.com>
2021-05-07 16:02:56 +0800

    Added Load Scene System for lvls 1-2

---
+++ Assets/Scripts/ToContinue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ToContinue : MonoBehaviour {
+
+	private int sceneToContinue;
+
+	public void ContinueGame(){
+		sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+
+		if (sceneToContinue != 0){
+			SceneManager.LoadScene(sceneToContinue);
+		}else{
+			return;
+		}
+	}
+}
