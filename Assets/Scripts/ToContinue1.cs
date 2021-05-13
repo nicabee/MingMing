60ac949b268c9caf4ac2aa4847b2fe4faeef43e1
Monica <monicabee358@gmail.com>
2021-05-14 01:13:40 +0800

    fixed audio pause for backstory scenes

--- Assets/Scripts/ToContinue1.cs
+++ Assets/Scripts/ToContinue1.cs
@@ -14,16 +14,21 @@ public class ToContinue1 : MonoBehaviour {
 		if (sceneToContinue != 0){
 			SceneManager.LoadScene(sceneToContinue);
           //  Debug.Log(sceneToContinue);
-           if(sceneToContinue == 7){
+            if(sceneToContinue == 3 || sceneToContinue == 4){
+                DontDestroyMusic.Instance.gameObject.GetComponent<AudioSource>().Play();
+            }
+            else if(sceneToContinue == 5 || sceneToContinue == 6 || sceneToContinue == 7 || sceneToContinue == 8){
                 DontDestroyMusicLvl1.Instance.gameObject.GetComponent<AudioSource>().Play();
-            }else if (sceneToContinue == 10){
+            }else if (sceneToContinue == 9 || sceneToContinue == 10 || sceneToContinue == 11){
                 DontDestroyMusicLvl2.Instance.gameObject.GetComponent<AudioSource>().Play();
-            }else if (sceneToContinue == 13){
+            }else if (sceneToContinue == 12 || sceneToContinue == 13 || sceneToContinue == 14){
                 DontDestroyMusicLvl3.Instance.gameObject.GetComponent<AudioSource>().Play();
-            }else if (sceneToContinue == 16){
+            }else if (sceneToContinue == 15 || sceneToContinue == 16 || sceneToContinue == 17){
                 DontDestroyMusicLvl4.Instance.gameObject.GetComponent<AudioSource>().Play();
-            }else if (sceneToContinue == 19){
+            }else if (sceneToContinue == 18 || sceneToContinue == 19 || sceneToContinue == 20){
                 DontDestroyMusicLvl5.Instance.gameObject.GetComponent<AudioSource>().Play();
+            }else if (sceneToContinue == 21 || sceneToContinue == 22 ){
+            DontDestroyMusicLvl6.Instance.gameObject.GetComponent<AudioSource>().Play();
             }
                 
        
--- Assets/Scripts/ToMenu1.cs
+++ Assets/Scripts/ToMenu1.cs
@@ -13,16 +13,21 @@ public class ToMenu1 : MonoBehaviour {
 		currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 		PlayerPrefs.SetInt("SavedScene", currentSceneIndex);
         //Debug.Log(currentSceneIndex);
-        if(currentSceneIndex == 7){
+        if(currentSceneIndex == 3 || currentSceneIndex == 4){
+            DontDestroyMusic.Instance.gameObject.GetComponent<AudioSource>().Pause();
+        }
+        else if(currentSceneIndex == 5 || currentSceneIndex == 6 || currentSceneIndex == 7 || currentSceneIndex == 8){
             DontDestroyMusicLvl1.Instance.gameObject.GetComponent<AudioSource>().Pause();
-        }else if (currentSceneIndex == 10){
+        }else if (currentSceneIndex == 9 || currentSceneIndex == 10 || currentSceneIndex == 11){
             DontDestroyMusicLvl2.Instance.gameObject.GetComponent<AudioSource>().Pause();
-        }else if (currentSceneIndex == 13){
+        }else if (currentSceneIndex == 12 || currentSceneIndex == 13 || currentSceneIndex == 14){
             DontDestroyMusicLvl3.Instance.gameObject.GetComponent<AudioSource>().Pause();
-        }else if (currentSceneIndex == 16){
+        }else if (currentSceneIndex == 15 || currentSceneIndex == 16 || currentSceneIndex == 17){
             DontDestroyMusicLvl4.Instance.gameObject.GetComponent<AudioSource>().Pause();
-        }else if (currentSceneIndex == 19){
+        }else if (currentSceneIndex == 18 || currentSceneIndex == 19 || currentSceneIndex == 20){
             DontDestroyMusicLvl5.Instance.gameObject.GetComponent<AudioSource>().Pause();
+        }else if (currentSceneIndex == 21 || currentSceneIndex == 22 ){
+            DontDestroyMusicLvl6.Instance.gameObject.GetComponent<AudioSource>().Pause();
         }
 
 		SceneManager.LoadScene(0);
